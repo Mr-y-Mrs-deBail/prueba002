b99cabd070a4452da436c40edb9cfef06d8c50ba
Mr-y-Mrs-deBail <142449717+Mr-y-Mrs-deBail@users.noreply.github.com>
2025-09-26 14:45:07 -0600

    MV

--- java.js
+++ java.js
@@ -55,45 +55,29 @@ document.addEventListener('DOMContentLoaded', () => {
         audioPlayer.play();
     };
 
-    const updateMediaSession = (song) => {
-        if ('mediaSession' in navigator) {
-            navigator.mediaSession.metadata = new MediaMetadata({
-                title: song.title,
-                artist: song.artist,
-                album: song.album,
-                 artwork: [
-      {
-        src: "https://dummyimage.com/96x96",
-        sizes: "96x96",
-        type: "image/png",
-      },
-      {
-        src: "https://dummyimage.com/128x128",
-        sizes: "128x128",
-        type: "image/png",
-      },
-      {
-        src: "https://dummyimage.com/192x192",
-        sizes: "192x192",
-        type: "image/png",
-      },
-      {
-        src: "https://dummyimage.com/256x256",
-        sizes: "256x256",
-        type: "image/png",
-      },
-      {
-        src: "https://dummyimage.com/384x384",
-        sizes: "384x384",
-        type: "image/png",
-      },
-      {
-        src: "https://dummyimage.com/512x512",
-        sizes: "512x512",
-        type: "image/png",
-      },
-                ],
-            });
+   const updateMediaSession = (song) => {
+    if ('mediaSession' in navigator) {
+        const artworkUrl = song.artwork;
+
+        navigator.mediaSession.metadata = new MediaMetadata({
+            title: song.title,
+            artist: song.artist,
+            album: song.album,
+            artwork: [
+                { src: artworkUrl, sizes: "96x96", type: "image/jpeg" },
+                { src: artworkUrl, sizes: "192x192", type: "image/jpeg" },
+                { 
+                    src: artworkUrl, 
+                    sizes: "700x700", 
+                    type: "image/jpeg" 
+                },
+                { 
+                    src: artworkUrl, 
+                    sizes: "1400x1400", 
+                    type: "image/jpeg" 
+                }
+            ],
+        });
 
             navigator.mediaSession.setActionHandler('play', () => audioPlayer.play());
             navigator.mediaSession.setActionHandler('pause', () => audioPlayer.pause());
